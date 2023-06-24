473a84b88dc44c46ee26e755f2deff88d1c286aa
Carson Ng <ngcarson@gmail.com>
2023-06-24 17:38:48 -0600

    First push

--- src/App.js
+++ src/App.js
@@ -1,23 +1,61 @@
 import logo from './logo.svg';
 import './App.css';
+import { Component } from 'react';
+import { render } from '@testing-library/react';
+
+let defaultTextColor = '#fff'
+let defaultStyle = {
+  color: defaultTextColor
+};
+
+class Aggregate extends Component {
+  render() {
+    return (
+      <div style={{...defaultStyle, width: "40%", display: 'inline-block'}}>
+        <h2 style={{color: defaultTextColor}}>Number Text</h2>
+      </div>
+    );
+  }
+}
+
+class Filter extends Component {
+  render() {
+    return (
+      <div style={{color: defaultTextColor}}>
+        <img></img>
+        <input type="text"></input>
+        Filter
+      </div>
+    )
+  }
+}
+
+class Playlist extends Component {
+  render() {
+    return (
+      <div style={{...defaultStyle, display: 'inline-block', width: "25%"}}>
+        <img />
+        <h3>Playlist Name</h3>
+        <ul><li>Song 1</li><li>Song 2</li><li>Song 3</li><li>Song 4</li></ul>
+      </div>
+    )
+  }
+}
 
 function App() {
+  let name = 'Carson'
+  let green = '#FF1212'
+  let headerStyle = { color: green, 'font-size': '50px' }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Title</h1>
+      <Aggregate/>
+      <Aggregate/>
+      <Filter/>
+      <Playlist/>
+      <Playlist/>
+      <Playlist/>
+      <Playlist/>
     </div>
   );
 }
